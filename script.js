c204ca89750dfab158772380a20a2742737cee1b
Vagner Gonçalves <vagner_lima@hst.com.br>
2020-07-31 14:52:49 -0300

    Arquivo JavaScript para dar dinamismo à página

---
+++ script.js
@@ -0,0 +1,216 @@
+var i = 0;
+function ola()
+{
+	if(i ==0)
+	{
+		var el = document.getElementById("dem").innerHTML = "<p>Produto versátil que oferece funcionalidade e estilo para ambientes residenciais, comerciais, repartições públicas e etc. Laminas em 89mm, com uma variedade de cores e padrões, se enquadra perfeitamente dentro de sua decoração, possibilitando uma escolha ampla para decorar qualquer tipo de ambiente. Possibilitando ao cliente fornecimento, manutenção e limpeza.<p>";
+		i++;
+	}
+	else
+	{
+		document.getElementById("dem").innerHTML = "<p></p>";
+		i--;
+	}
+	
+    
+  
+}
+
+document.getElementById("oi").onclick = ola;
+
+var j = 0;
+function ola2()
+{
+	if(j ==0)
+	{
+		document.getElementById("dem2").innerHTML = "<p>As persianas horizontais são as que mais tempo estão no mercado, sendo encontradas em ambas as finalidades, sejam elas comerciais ou residenciais, nas versões em alumínio de 25mm e 16mm, ou até mesmo em imitações elegantes de madeiras, para aqueles que pretendem ter um ambiente mais personalizado. Possibilitando ao cliente fornecimento, manutenção e limpeza</p>";
+		j++;
+	}
+	else
+	{
+		document.getElementById("dem2").innerHTML = "<p></p>";
+		j--;
+	}
+	
+    
+  
+}
+
+document.getElementById("Hori").onclick = ola2;
+
+var j = 0;
+function ola2()
+{
+	if(j ==0)
+	{
+		document.getElementById("dem2").innerHTML = "<p>As persianas horizontais são as que mais tempo estão no mercado, sendo encontradas em ambas as finalidades, sejam elas comerciais ou residenciais, nas versões em alumínio de 25mm e 16mm, ou até mesmo em imitações elegantes de madeiras, para aqueles que pretendem ter um ambiente mais personalizado. Possibilitando ao cliente fornecimento, manutenção e limpeza</p>";
+		j++;
+	}
+	else
+	{
+		document.getElementById("dem2").innerHTML = "<p></p>";
+		j--;
+	}
+	
+    
+  
+}
+
+document.getElementById("Hori").onclick = ola2;
+
+ j = 0;
+function ola3()
+{
+	if(j ==0)
+	{
+		document.getElementById("dem3").innerHTML = "<p>As cortinas rolô vêm alcançando cada vez mais um espaço dentro do mercado, conquistando grande força comercial pelo aspecto de praticidade e beleza que se enquadra com muita facilidade em qualquer decoração.Possui mecanismo de fácil manuseio, tecidos enroláveis que deixam o ambiente livre, até mesmo podendo ficar invisível em cortineiros, aspecto esse que agrada muitos arquitetos e designers de interiores. Possibilitando ao cliente fornecimento, manutenção e limpeza.</p>";
+		j++;
+	}
+	else
+	{
+		document.getElementById("dem3").innerHTML = "<p></p>";
+		j--;
+	}
+	
+    
+  
+}
+
+document.getElementById("rolo").onclick = ola3;
+
+j = 0;
+function ola4()
+{
+	if(j ==0)
+	{
+		document.getElementById("dem4").innerHTML = "<p>As persianas do estilo Romana se destacam por seu estilo diferenciado que se apresenta em gomos, podendo ser recolhida em partes, obtendo assim, um charme clássico desse modelo. Agregando a qualquer ambiente um aspecto elegante a decoração. Possibilitando ao cliente fornecimento, manutenção e limpeza.</p>";
+		j++;
+	}
+	else
+	{
+		document.getElementById("dem4").innerHTML = "<p></p>";
+		j--;
+	}
+	
+    
+  
+}
+
+document.getElementById("Romana").onclick = ola4;
+
+j = 0;
+function ola5()
+{
+	if(j ==0)
+	{
+		document.getElementById("dem5").innerHTML = "<p>As persianas Double Vision são modernas e elegantes e são as cortinas decorativas que apresentam mais sofisticação no momento. Sendo a preferência em destaque para arquitetos e decoradores, pois além de seu designer diferenciado, permitem um ajuste para entrada de iluminação, obtendo assim, uma decoração elegante ao espaço. Possibilitando ao cliente fornecimento, manutenção e limpeza.</p>";
+		j++;
+	}
+	else
+	{
+		document.getElementById("dem5").innerHTML = "<p></p>";
+		j--;
+	}
+	
+    
+  
+}
+
+document.getElementById("Vision").onclick = ola5;
+
+j = 0;
+function Banheiro()
+{
+	if(j ==0)
+	{
+		document.getElementById("Amb1").innerHTML = "<p>Com muito bom gosto, todo ambiente fica aconchegante e convidativo, principalmente na ora de bem-estar, podendo se transformar em um local moderno.<br><br>"+
+		"Aplicações: <br> Box, Janelas, espelhos, nichos e revestimentos.<br>"+
+		"Box: Vidro Temperado de 6, 8 e 10 mm, liso nas cores verde, incolor, cinza e bronze."+
+		"Nichos: Vidro Temperado de 10, 15 e 18mm, incolor liso. Espelhos de 4 e 6 mm, bisotado ou lapidado. Revestimento de vidro decorativo de 4 e 6 mm, diversas cores para decorar qualquer ambiente.</p>";
+
+
+		j++;
+	}
+	else
+	{
+		document.getElementById("Amb1").innerHTML = "<p></p>";
+		j--;
+	}
+	
+    
+  
+}
+
+document.getElementById("Banheiro").onclick = Banheiro;
+
+j = 0;
+function Dormitorio()
+{
+	if(j ==0)
+	{
+		document.getElementById("Amb2").innerHTML = "<p>Esse é o local onde podemos ter a maior sensação de tranquilidade, associando o descanso ao prazer em ter um ambiente convidativo e aconchegante.<br><br>"+ 
+													"Aplicações:<br>"+
+													"Janelas (comuns ou acústicas), Portas, Paredes decorativas, aparadores, racks e etc.Espelhos de 4 e 6 mm, bisotado ou lapidadoVidro decorativo de 4 e 6 mm, diversas cores para decorar qualquer ambiente.</p>";
+
+
+		j++;
+	}
+	else
+	{
+		document.getElementById("Amb2").innerHTML = "<p></p>";
+		j--;
+	}
+	
+    
+  
+}
+
+document.getElementById("Dormi").onclick = Dormitorio;
+
+j = 0;
+function Salas()
+{
+	if(j ==0)
+	{
+		document.getElementById("Amb3").innerHTML = "<p>Com muita criatividade e bom gosto, as salas de visita e jantar agregam e transformam o ambiente transformando em um local aconchegante e convidativo a qualquer usuário, garantindo uma boa recepção.<br><br>"+ 
+													"Aplicações:<br>Portas, janelas, mobiliários, tampos de vidro para mesas, revestimentos para paredes, fechamento de sacadas e guarda-corpo.<p>";
+
+
+
+		j++;
+	}
+	else
+	{
+		document.getElementById("Amb3").innerHTML = "<p></p>";
+		j--;
+	}
+	
+    
+  
+}
+
+document.getElementById("Salas").onclick = Salas;
+
+j = 0;
+function Escadas()
+{
+	if(j ==0)
+	{
+		document.getElementById("Amb4").innerHTML = "<p>Instalação de corrimãos responsáveis por assegurar e proteger pessoas que utilizam escadas e rampas. Possuindo diversos materiais de acordo com o projeto do cliente.<br><br>"+
+													"Aplicações:<br>Aço inox, latão, vidros temperados com espessuras diversas, conforme projeto.</p>"
+
+
+		j++;
+	}
+	else
+	{
+		document.getElementById("Amb4").innerHTML = "<p></p>";
+		j--;
+	}
+	
+    
+  
+}
+
+document.getElementById("Escadas").onclick = Escadas;
+
